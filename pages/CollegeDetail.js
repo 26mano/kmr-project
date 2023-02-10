5002362006f2efd9a7575b23224830f1b715ea5b
26mano <[mmanoranjith1997@gmail.com]>
2023-02-10 19:29:04 +0530

    first commit

---
+++ pages/CollegeDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CollegeDetail = () => {
+  return (
+    <div>CollegeDetail</div>
+  )
+}
+
+export default CollegeDetail
---
+++ pages/CollegeListing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CollegeListing = () => {
+  return (
+    <div>CollegeListing</div>
+  )
+}
+
+export default CollegeListing
