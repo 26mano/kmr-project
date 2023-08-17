62c1a1f7520c340ea0d7372443dc7fb95d88cf63
26mano <103581969+26mano@users.noreply.github.com>
2023-08-17 16:33:33 +0530

    Add files via upload

--- pages/[products]/index.js
+++ pages/[products]/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Avatar,
   Box,
   Card,
   CardContent,
@@ -10,11 +11,12 @@ import {
   Typography,
 } from "@mui/material";
 import Link from "next/link";
-import Card2 from "../../components/Card/Card2";
-
+import Image from "next/image";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import { LocationOn } from "@mui/icons-material";
+// import {Card2} from "../../components/Card/Card2";
 
 export default function api({ detail, post1 }, props) {
-
   return (
     <Stack alignItems="center" height="100%">
       <Grid container display="flex" spacing={4} mt={4}>
@@ -53,21 +55,123 @@ export default function api({ detail, post1 }, props) {
       <Grid container display="flex" spacing={2} mt={4}>
         {post1.map((element, i) => {
           return (
-            <Grid item lg={3} key={i} alignItems="normal"
-            height="100%" >
-              <Card2
-                CollegeLogo={element.logo}
-                CollegeImage={element.featured_image}
-                CollegeName={element.institute_name}
-                CollegeDistrict={element.district_name}
-                CollegeState={element.state_name}
-                // Courses={element.course.map((e) => {  if (e.short_name != "") {return (`${e.short_name} | `); }} )}
-                AdmissionFee={element.fees}
-                Rating={element.rating}
-                Kms={element.dmr}
-                sx={{ width: 280 }}
-                href={`/colleges/${element.id}-${element.institute_name?.replace(/[ ]/g,"-")}`}
-              />
+            <Grid item lg={3} key={i} alignItems="normal" height="100%">
+              <Card
+                sx={{
+                  height: "100%",
+                  width: 280,
+                  bgcolor: "common.white",
+                  display: "grid",
+                  color: "common.black",
+                  borderRadius: "13px",
+                  alignItems: "normal",
+                }}
+                // onClick={onClick}
+              >
+                <Box>
+                  <CardMedia
+                    component="img"
+                    height="150"
+                    image={`${element.featured_image}`}
+                    alt={element.institute_name}
+                  />
+                </Box>
+
+                <CardHeader
+                  sx={{ display: "flex" }}
+                  avatar={
+                    <Avatar
+                      sx={{
+                        width: 60,
+                        height: 60,
+                        mt: -8,
+                        borderRadius: "50px",
+                        boxShadow: "1px 1px 1px 1px grey",
+                      }}
+                      // imgProps={`https://w.kalvimalar.com/images/${CollegeLogo}`}
+                    >
+                      {/* <img src={`https://w.kalvimalar.com/images/${CollegeLogo}`} /> */}
+                      <Image src={`${element.logo}`} width={55} height={55} />
+                    </Avatar>
+                  }
+                  title={
+                    <Box
+                      sx={{
+                        display: "flex",
+                        ml: 13.7,
+                        mt: -3,
+                        position: "relative",
+                      }}
+                    >
+                      <StarRateIcon
+                        sx={{
+                          bgcolor: "grey.200",
+                          // bgcolor:themeMode === true ? "grey.800":'goldenrod',
+                          color: "goldenrod",
+                          // color: themeMode === true ? "goldenrod" :"common.white",
+                          borderRadius: "20px",
+                          boxShadow: "2px 1px 1px rgb(0,0,0,0.2)",
+                          position: "absolute",
+                          ml: -2,
+                          mt: -0.3,
+                          width: 27,
+                          height: 27,
+                          zIndex: "9999px !important",
+                          p: "4px",
+                          // color: "#F0A500"
+                        }}
+                      />
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          color: "grey.500",
+                          bgcolor: "grey.300",
+                          fontWeight: 600,
+                          borderRadius: "0px 10px 10px 0px",
+                          px: "6px",
+                          pl: 2,
+                        }}
+                      >
+                        {(element.rating / 5).toString().slice(0, 3)}
+                      </Typography>
+                    </Box>
+                  }
+                />
+                <CardContent
+                  sx={{ mt: -1, display: "grid", justifyContent: "left" }}
+                >
+                  {
+                    <>
+                      <Link href={`/colleges/${element.id}-${element.institute_name?.replace(/[ ]/g, "-")}`}>
+                        <Typography
+                          variant="title"
+                          sx={{ color: "primary.dark" }}
+                        >
+                          {element.institute_name}
+                        </Typography>
+                      </Link>
+                    </>
+                  }
+                  {
+                    <>
+                      <Typography
+                        variant="caption"
+                        sx={{ display: "flex", color: "grey.600" }}
+                        gutterBottom
+                      >
+                        <LocationOn
+                          sx={{
+                            fontSize: "14px",
+                            pr: "2px",
+                            color: "grey.600",
+                          }}
+                        />
+                        {element.district_name},{element.state_name}
+                      </Typography>
+                    </>
+                  }
+                </CardContent>
+              </Card>
             </Grid>
           );
         })}
