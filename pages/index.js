2cdef9bb90b0affbd2a2ddc28b292b1afa38b8e3
26mano <103581969+26mano@users.noreply.github.com>
2023-08-17 17:26:13 +0530

    index

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,90 @@
-import React from "react";
-import {
-  Avatar,
-  Box,
-  Card,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Grid,
-  Stack,
-  Typography,
-} from "@mui/material";
-import Link from "next/link";
-import Image from "next/image";
-import { StarRate as StarRateIcon} from "@mui/icons-material";
-import { LocationOn } from "@mui/icons-material";
-// import {Card2} from "../../components/Card/Card2";
+import React from 'react'
+import { Box, Card, CardContent, CardHeader, CardMedia, Grid, Stack, Typography } from '@mui/material';
 
-export default function index({ detail}, props) {
+export default function api({ detail, post1 }, props) {
+
   return (
-    <Stack alignItems="center" height="100%">
-      <Grid container display="flex" spacing={4} mt={4}>
-        {detail.map((e, i) => {
-          return (
-            <Grid item lg={3} key={i}>
-              <Card>
-                <CardHeader
-                  title={
-                    <Box display="flex" justifyContent="space-between">
-                      <Typography variant="subtitle1">{e.title}</Typography>
-                      <Typography>{e.rating}/5</Typography>
-                    </Box>
-                  }
-                  subheader={
-                    <>
-                      <Typography variant="body1">{e.description}</Typography>
-                    </>
-                  }
-                />
-                <CardMedia component="img" height="155" image={e.thumbnail} />
-                <CardContent>
-                  <Link href={`/products/${e.brand}`}>
-                    <Box display="flex" justifyContent="space-between">
-                      <Typography> Brand : {e.brand}</Typography>
-                      <Typography> Price : {e.price} dollar</Typography>
-                    </Box>
-                  </Link>
-                </CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
+    <Stack alignItems="center" height="100vh" >
+      <Grid container display="flex" spacing={4} mt={4} >
+      {detail.map((e , i) => {
+        return(
+      <Grid item lg={3 } key={i} >
+      <Card>
+        <CardHeader
+          title={
+            <Box display="flex" justifyContent='space-between' >
+            <Typography variant="subtitle1" >{e.title}</Typography>
+            <Typography>{e.rating}/5</Typography>
+            </Box>
+          }
+          subheader={
+            <>
+            <Typography variant='body1' >{e.description}</Typography>
+            </>
+          }
+         />
+         <CardMedia 
+         component="img"
+         height="155"
+         image={e.thumbnail}
+         />
+        <CardContent >
+          <Box display="flex" justifyContent="space-between" >
+          <Typography> Brand : {e.brand}</Typography>
+          <Typography> Price : {e.price} dollar</Typography>
+          </Box>
+         
+        </CardContent>
+      </Card>
+          </Grid>
+        )
+      })}
       </Grid>
-      {/* -------------------- */}
-     
+      <Grid container display="flex" spacing={4} mt={4} >
+      {post1.map((e , i) => {
+        return(
+      <Grid item lg={3 } key={i} >
+      <Card>
+        <CardHeader
+          title={
+            <Box display="flex" justifyContent='space-between' >
+            <Typography variant="subtitle1" >{e.title}</Typography>
+            <Typography>{e.rating}/5</Typography>
+            </Box>
+          }
+         />
+         {/* <CardMedia 
+         component="img"
+         height="155"
+         image={e.thumbnail}
+         /> */}
+        <CardContent >
+          <Box display="flex" justifyContent="space-between" >
+          <Typography> Brand : {e.body}</Typography>
+          {/* <Typography> Price : {e.price} dollar</Typography> */}
+          </Box>
+         
+        </CardContent>
+      </Card>
+          </Grid>
+        )
+      })}
+      </Grid>
+    
     </Stack>
-  );
+  )
 }
 
-export const getServerSideProps = async (context) => {
-  let get = await fetch(`https://dummyjson.com/products`);
-  let res = await get.json();
-  let detail = res.products;
 
-  return {
-    props: { detail },
-  };
-};
+
+export const getServerSideProps = async () => {
+    let get = await fetch (`https://dummyjson.com/products`);
+    let post = await fetch (`https://dummyjson.com/posts`);
+    let res = await get.json();
+    let res1 = await post.json();
+    let detail = res.products;
+    let post1 = res1.posts;
+    return {
+      props : {detail , post1}
+    }
+}
